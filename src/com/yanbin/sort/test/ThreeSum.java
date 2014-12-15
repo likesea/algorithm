b2751be7643a3059de513289efed0d7b1663edfc
likesea <kukuyanbin@163.com>
2014-12-15 21:19:25 +0800

    aa

---
+++ src/com/yanbin/sort/test/ThreeSum.java
@@ -0,0 +1,25 @@
+package com.yanbin.sort.test;
+
+import java.util.Random;
+
+import com.yanbin.util.StdOut;
+
+public class ThreeSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = getInts(10);
+		for (int i = 0; i < array.length; i++) {
+			StdOut.println(array[i]);
+		}
+	}
+	private static int[] getInts(int num) {
+		int[] intArray = new int[num];
+		Random random = new Random();
+		for (int i = 0; i < num; i++) {
+			intArray[i]=random.nextInt(1000);
+		}
+		return intArray;
+	}
+
+}
